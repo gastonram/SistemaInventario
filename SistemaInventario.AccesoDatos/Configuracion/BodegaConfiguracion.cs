c6cf8927d54ef2411edbfabd5c74126f88bc88d7
Masta63 <gaston_63ramos@hotmail.com>
2024-07-24 16:15:43 -0300

    Agrego tabla bodega y migracion

---
+++ SistemaInventario.AccesoDatos/Configuracion/BodegaConfiguracion.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SistemaInventario.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.AccesoDatos.Configuracion
+{
+    public class BodegaConfiguracion : IEntityTypeConfiguration<Bodega>
+    {
+
+        public void Configure(EntityTypeBuilder<Bodega> builder)
+        {
+            builder.HasKey(b => b.Id);
+            builder.Property(b => b.Nombre).IsRequired().HasMaxLength(60);
+            builder.Property(b => b.Descripcion).IsRequired().HasMaxLength(120);
+            builder.Property(b => b.Estado).IsRequired();
+        }
+    }
+}
