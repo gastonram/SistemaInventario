42ec1e3d3f0a9ad582aac21f8b25fa6192892a0d
Masta63 <gaston_63ramos@hotmail.com>
2024-07-24 16:01:47 -0300

    agrego el modelo bodega en modelos y el dbcontext en acceso datos

---
+++ SistemaInventario.Modelos/Bodega.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.Modelos
+{
+
+    
+    public class Bodega
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required (ErrorMessage ="El nombre es un atributo requerido")]
+        [MaxLength(60, ErrorMessage = "El nombre no puede tener mas de 50 caracteres")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "La descripcion es un atributo requerido")]
+        [MaxLength(120, ErrorMessage = "la descripcion no puede tener mas de 120 caracteres")]
+        public string Descripcion { get; set;}
+
+        [Required(ErrorMessage = "El estado es un atributo requerido")]
+        public bool Estado { get; set;}
+    }
+}
