998272abffa096c8cea8811caf2ce8bb90d2e289
Masta63 <gaston_63ramos@hotmail.com>
2024-07-26 19:28:46 -0300

    Agrego modelo y db context de Producto

---
+++ SistemaInventario.AccesoDatos/Configuracion/ProductoConfiguracion.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SistemaInventario.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.AccesoDatos.Configuracion
+{
+    public class ProductoConfiguracion : IEntityTypeConfiguration<Producto>//aca le decimos que utilice el modelo que creamos
+    {
+
+        public void Configure(EntityTypeBuilder<Producto> builder)
+        {
+            builder.HasKey(b => b.Id);
+            builder.Property(b => b.NumeroSerie).IsRequired().HasMaxLength(60);
+            builder.Property(b => b.Descripcion).IsRequired().HasMaxLength(150);
+            builder.Property(b => b.Estado).IsRequired();
+            builder.Property(b => b.Precio).IsRequired();
+            builder.Property(b => b.Costo).IsRequired();
+            builder.Property(b => b.CategoriaId).IsRequired();
+            builder.Property(b => b.MarcaId).IsRequired();
+            builder.Property(b => b.ImagenUrl).IsRequired(false);
+            builder.Property(b => b.PadreId).IsRequired(false);
+
+            /*relaciones*/
+            builder.HasOne(b => b.Categoria).WithMany()
+                   .HasForeignKey(b => b.CategoriaId)
+                   .OnDelete(DeleteBehavior.NoAction);
+
+            builder.HasOne(b => b.Marca).WithMany()
+                   .HasForeignKey(b => b.MarcaId)
+                   .OnDelete(DeleteBehavior.NoAction);
+
+            builder.HasOne(b => b.Padre).WithMany()
+                   .HasForeignKey(b => b.PadreId)
+                   .OnDelete(DeleteBehavior.NoAction);
+        }
+    }
+}
---
+++ SistemaInventario.Modelos/Producto.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.Modelos
+{
+    public class Producto
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="El Numero de Serie es Requerido")]
+        [MaxLength(60,ErrorMessage ="El valor maximo es de 60 caracteres")]
+        public string NumeroSerie { get; set; }
+
+        [Required(ErrorMessage = "La Descripcion es Requerido")]
+        [MaxLength(150, ErrorMessage = "El valor maximo es de 150 caracteres")]
+        public string Descripcion { get; set; }
+
+        
+        [Required(ErrorMessage = "El Precio es Requerido")]
+        public double Precio { get; set; }
+
+        [Required(ErrorMessage = "El Costo es Requerido")]
+        public double Costo { get; set; }
+
+        public string ImagenUrl { get; set; }
+
+        public bool Estado { get; set; }
+
+        [Required(ErrorMessage = "La Categoria es Requerida")]
+        public int CategoriaId { get; set; }
+
+        [ForeignKey("CategoriaId")]
+        public Categoria Categoria { get; set; }
+
+        [Required(ErrorMessage = "La Marca es Requerida")]
+        public int MarcaId { get; set; }//para relacionarlos con la tabla marca
+
+        [ForeignKey("MarcaId")]//para relacionarlos con la tabla marca debe ser exactamente el mismo nombre que la propiedad
+        public Marca Marca { get; set; }
+
+        public int? PadreId { get; set; }//una propiedad padre que se relacione con la misma tabla de mi modelo producto
+                                         //, necesitamos que se grabe como null en la base de datos porque sino se guarda como 0 y trae problemas
+        public virtual Producto Padre { get; set; }//relacion con la misma tabla de producto, recursividad. un producto puede estar relacionado a un mismo producto
+
+
+    }
+}
